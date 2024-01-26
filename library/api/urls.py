d1ddcc4dc007e3b71ae2bcbf33e6b74b42c94442
Oğuzhan Öksel <oguzhanoksel@hotmail.com>
2024-01-26 22:40:59 +0300

    Library Website and API

---
+++ library/api/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from api.views import BookAPIView
+
+
+urlpatterns = [
+    path('', BookAPIView.as_view())
+]
---
+++ library/api/views.py
@@ -0,0 +1,9 @@
+from rest_framework import generics
+from api.serializers import BookSerializer
+
+from books.models import Book
+
+
+class BookAPIView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
